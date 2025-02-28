626b1f1c59788361006477e759fa754124f50614
Pedrooaj <133423641+Pedrooaj@users.noreply.github.com>
2025-02-28 16:14:11 -0300

    Update EnviarPacote.ts

--- src/events/EnviarPacote.ts
+++ src/events/EnviarPacote.ts
@@ -3,9 +3,26 @@ class EnviarPacote{
     public destino: string = "";
     public data: string = new Date().toLocaleDateString("pt-BR");
     public descricao: string = "";
+    public estado: number = 0;
 
     start = async (msg: Message) => {
-        const mensagem: string = msg.body.toLocaleLowerCase();
+    const mensagem: string = msg.body.toLocaleLowerCase();
+    if(this.estado == 0){
+        msg.reply("Digite o Destino do seu envio: ");
+        this.destino = mensagem;
+        this.estado += 1;
+    }else if(this.estado == 1){
+        msg.reply(`Deseja enviar o pacote para o Destino: *${this.destino}*\n(S)im \n(N)ão`);
+        if(mensagem === "s" || mensagem === "sim"){
+            this.estado += 1;
+        }else{
+            msg.reply("Ops... Valor inválido operação cancelada!")
+            this.estado -= 1;
+        }
+    }else if(this.estado == 2){
+        msg.reply("continua...")
+    }
+
         
     }
 
